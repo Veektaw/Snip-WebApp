d13bb5402fff5f924c459903dbac467adaeb39bc
Victor Ose Iyayi <veektaw@gmail.com>
2023-06-21 17:47:31 +0100

    added copy buttons

--- app/snipe/static/app.js
+++ app/snipe/static/app.js
@@ -1,12 +1,16 @@
+// Selecting elements
 const input = document.querySelector("#input-field");
-const shortUrl = document.querySelector("#new-url");
+const shortUrl = document.querySelector("#new-url-label .new-url-result");
+const shortUrlhistory = document.querySelector(".short-url-history");
 const clearButton1 = document.querySelector("#clear-btn");
 const clearButton2 = document.querySelector("#clear-btn2");
-const copyButton = document.querySelector("#copy-btn[data-clipboard-target='#new-url-label']");
-const copyButtons = document.querySelectorAll(".copy-btn2[data-clipboard-target]");
+const copyButton1 = document.querySelector("#copy-btn");
+const copyButton2 = document.querySelector("#copy-btn1");
+const copyButton5 = document.querySelector("#copy-btn5");
 const inputLongUrl = document.querySelector(".text-field-content-row2[name='long_url']");
 const inputCustomUrl = document.querySelector(".text-field-content-row2[name='custom_url_entry']");
-const customUrlResult = document.querySelector("#new-url-label .new-url-result");
+const customUrlResult = document.querySelector("#custom-url-created");
+const errorDiv = document.querySelector("#error-div");
 
 // Clear fields function
 const clearFields = () => {
@@ -18,7 +22,7 @@ const clearFields = () => {
 const clearFields2 = () => {
   inputLongUrl.value = '';
   inputCustomUrl.value = '';
-  hideResult();
+  hideCustomUrlResult();
   hideError();
 };
 
@@ -41,6 +45,14 @@ const hideResult = () => {
   shortUrl.style.display = "none";
 };
 
+const showCustomUrlResult = () => {
+  customUrlResult.style.display = "block";
+};
+
+const hideCustomUrlResult = () => {
+  customUrlResult.style.display = "none";
+};
+
 const showError = () => {
   errorDiv.style.display = "block";
 };
@@ -50,26 +62,43 @@ const hideError = () => {
 };
 
 // Copy button function
-copyButton.addEventListener("click", (event) => {
+const copyToClipboard = (text) => {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      console.log("Text copied to clipboard:", text);
+    })
+    .catch((error) => {
+      console.error("Failed to copy text:", error);
+    });
+};
+
+copyButton1.addEventListener("click", (event) => {
   event.preventDefault();
-  const range = document.createRange();
-  range.selectNode(customUrlResult);
-  window.getSelection().removeAllRanges();
-  window.getSelection().addRange(range);
-  document.execCommand("copy");
-  window.getSelection().removeAllRanges();
+  const textToCopy = shortUrl.textContent;
+  copyToClipboard(textToCopy);
 });
 
-copyButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    event.preventDefault();
-    const target = button.getAttribute("data-clipboard-target");
-    const range = document.createRange();
-    const element = document.querySelector(target);
-    range.selectNode(element);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand("copy");
-    window.getSelection().removeAllRanges();
+copyButton2.addEventListener("click", (event) => {
+  event.preventDefault();
+  const textToCopy = customUrlResult.textContent;
+  copyToClipboard(textToCopy);
+});
+
+const handleCopyButtonClick = (event) => {
+  event.preventDefault();
+  const button = event.target;
+  const targetId = button.getAttribute('data-clipboard-target');
+  const targetElements = document.querySelectorAll(targetId);
+
+  let textToCopy = "";
+  targetElements.forEach((element) => {
+    textToCopy += element.textContent + "\n";
   });
+
+  copyToClipboard(textToCopy.trim());
+};
+
+const copyButtons = document.querySelectorAll('.copy-btn5');
+copyButtons.forEach((button) => {
+  button.addEventListener('click', handleCopyButtonClick);
 });
