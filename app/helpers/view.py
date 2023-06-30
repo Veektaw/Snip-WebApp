d6746f6d946e4c565ee97e2b6a51ec2d3a724c0b
Iyayi Victor <veektaw@gmail.com>
2023-06-30 18:50:38 +0100

    Update view.py this time, lol

--- app/helpers/view.py
+++ app/helpers/view.py
@@ -76,7 +76,7 @@ class URLCreator:
             r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or IP
             r'(?::\d+)?'  # optional port
             r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-        return bool(regex.match(url)) 
+        return (regex.match(url)) 
 
     @classmethod
     def extract_url_data(cls, url): 
