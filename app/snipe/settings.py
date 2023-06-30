06daf7386a3dc83874cd496b4cb48b51277dbef4
Iyayi Victor <veektaw@gmail.com>
2023-06-30 18:06:06 +0100

    Update settings.py ready

--- app/snipe/settings.py
+++ app/snipe/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 # base_dir = os.path.abspath(os.path.dirname(__file__))
 
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir,'snipe.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
